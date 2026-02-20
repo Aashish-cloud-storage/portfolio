9c6a5857d58b4634fe8e2394fc065e0ad8b09d24
Aashish-cloud-storage <aashishchauhan5935@gmail.com>
2026-02-20 19:40:05 +0530

    changes

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -15,48 +15,11 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Fresher</h4>
+                <h4 className="text-center"> Fresher </h4>
                
               </div>
-             
             </div>
-            {/* <p>
-              Worked as a Software Developer, contributing to the design, development, and maintenance of web-based applications.
-              Actively involved in building responsive user interfaces, implementing reusable components, and optimizing application performance.
-              <br /><br />
-              Collaborated closely with team members to understand project requirements, fix bugs, and deliver features within deadlines.
-              Gained hands-on experience with modern development tools, version control systems, and real-world development workflows.
-            </p> */}
-
           </div>
-          {/* <div className="career-info-box">
-            <div className="career-info-in">
-              <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
-              </div>
-              <h3>20XX</h3>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
-            </p>
-          </div>
-          <div className="career-info-box"> */}
-          {/* <div className="career-info-in">
-              <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
-              </div>
-              <h3>NOW</h3>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
-            </p>
-          </div> */}
         </div>
       </div>
     </div>
